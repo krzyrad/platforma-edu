dd62e4e4031c419d7a5e2dc1ab55b441c91da4e0
krzyrad <krzysztof.radon@protonmail.ch>
2019-01-23 13:50:37 +0100

    AWS S3 settings

--- projekt/settings.py
+++ projekt/settings.py
@@ -249,6 +249,15 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
 
     #Konfiguracja zasobów
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "") 
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+    AWS_QUERYSTRING_AUTH = False 
+    AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN", "")
+    MEDIA_ROOT = os.environ.get("MEDIA_URL", "")
+    MEDIA_URL = '/media/'
+
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = (
